67a041daa5f5d4d3574477e88a768adbfcac6041
Anders Markendahl <anders.markendahl@ericsson.com>
2023-05-26 15:35:49 +0200

    Fix Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import { jsx } from '@emotion/react';
 import { Footer } from './components/Footer';
 
 export const App = () => (
-  <div css={{ position: 'fixed' }}>
-    <Footer />
+  <div>
+    <div css={{
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        overflow: 'hidden',
+        marginBottom: '16px'
+      }}>
+      <Footer />
+    </div>
   </div>
 );

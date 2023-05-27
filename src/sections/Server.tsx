91c709f79f01fe174e19f1ab39e84fe9d27bca11
Anders Markendahl <anders.markendahl@ericsson.com>
2023-05-27 16:21:13 +0200

    Finalised Server

--- src/sections/Server.tsx
+++ src/sections/Server.tsx
@@ -18,11 +18,11 @@ interface ProjectItemProps {
 const ProjectItem = ({
   image, title, description, link, alt,
 }: ProjectItemProps) => (
-  <div css={{
-    padding: '4px', display: 'flex', alignItems: 'center', justifyContent: 'left',
-  }}
-  >
-    <Button sx={{ textTransform: 'none' }} href={link} color="inherit">
+  <Button sx={{ textAlign: 'left', textTransform: 'none' }} href={link} color="inherit">
+    <div css={{
+      padding: '4px', display: 'flex', alignItems: 'top', justifyContent: 'left',
+    }}
+    >
       <img alt={alt} src={image} width="80px" />
       <div css={{ display: 'block' }}>
         <Typography variant="h5">
@@ -32,8 +32,8 @@ const ProjectItem = ({
           {description}
         </Typography>
       </div>
-    </Button>
-  </div>
+    </div>
+  </Button>
 );
 
 export const Server = () => (

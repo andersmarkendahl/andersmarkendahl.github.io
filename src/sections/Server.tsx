2b94c4f8bd10090cfdf025bd2a6bf26999047679
Anders Markendahl <anders.markendahl@ericsson.com>
2023-05-27 18:56:39 +0200

    Fixed spacing in Server

--- src/sections/Server.tsx
+++ src/sections/Server.tsx
@@ -14,13 +14,13 @@ export const Server = () => (
         My Server
       </Typography>
       <Divider orientation="horizontal" />
+      </div>
       <Typography variant="body1">
         A server hosted by me and contain a changing set of services.
         Over the course of time the server have hosted Jenkins, TeamSpeak and
         various games such as Terraria, Minecraft, Starbound and Half-Life
         1 and 2 etc.
       </Typography>
-    </div>
     <ProjectItem image={serverImg} title="Server" description="Hosted Ubuntu Server, Apache, MySQL" link="" alt="" />
   </div>
 );

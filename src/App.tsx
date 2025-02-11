e82966bd1cbd761cb80fdd68f20562a50ae0fc98
Anders Markendahl <anders.markendahl@ericsson.com>
2025-02-11 19:32:27 +0100

    App.tsx trials for responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { jsx } from '@emotion/react'
 import { CssBaseline, Stack, Typography, ThemeProvider } from '@mui/material'
 import { profileImg } from './sections/images'
-import { primaryTheme } from './components/Theme'
+import { theme } from './components/Theme'
 import { Navigation } from './components/Navigation'
 import { Contact } from './sections/Contact'
 import { Server } from './sections/Server'
@@ -11,7 +11,7 @@ import { Skills } from './sections/Skills'
 import { Myself } from './sections/Myself'
 
 export const App = () => (
-    <ThemeProvider theme={primaryTheme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <div
         css={{
@@ -19,6 +19,9 @@ export const App = () => (
           alignItems: 'center',
           justifyContent: 'center',
           margin: '32px auto 160px auto',
+          padding: '0 16px',
+          minWidth: '320px',
+          maxWidth: '1200px',
           // Simple responsiveness
           '@media (min-width: 300px)': {
             maxWidth: '300px',
@@ -37,12 +40,20 @@ export const App = () => (
         <Stack spacing={12}>
           <div>
             <div id='home'>
-              <Typography variant='h1' align='center'>
+              <Typography variant='h2' align='center'>
                 Anders Markendahl
               </Typography>
             </div>
             <div css={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <img src={profileImg} alt='Anders profile' width='400px' />
+            <img
+              src={profileImg}
+              alt='Anders profile'
+              css={{
+                width: '100%',
+                maxWidth: '400px',
+                height: 'auto',
+              }}
+            />
             </div>
           </div>
           <Myself id='about' />
